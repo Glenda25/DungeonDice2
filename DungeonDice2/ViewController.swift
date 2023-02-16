58353dda31b71b5c3f0e026f51cbfab169a8e03f
Glenda Mullan <glenda.mullan@gmail.com>
2023-02-16 13:50:22 +1100

    Programming 1

--- DungeonDice2/ViewController.swift
+++ DungeonDice2/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var DungeonDiceLabel: UILabel!
+    
+    @IBOutlet weak var ChooseDiceLabel: UILabel!
+    
+    @IBOutlet weak var ResultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func FourSidedDice(_ sender: UIButton) {
+        //random choose a number between 1 and 4
+        let FourSideResult = Int.random(in: 1...4)
+        ResultsLabel.text = "You rolled the 4 sided dice and got a " + String(FourSideResult)
+    }
+    
 }
 
